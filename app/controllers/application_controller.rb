7c586fa2ebb1aeb3e3d4ee1544436756f3c1be5b
杉浦智大 <sugiuratomohiro@sugiurasatoshidainoMacBook-Pro.local>
2019-06-09 08:48:32 +0900

    userlogin

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,10 @@
 class ApplicationController < ActionController::Base
-  before_action :authenticate_user!
   protect_from_forgery with: :exception
+  before_action :authenticate_user!
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+  end
 end
