280eba9f0b8fc4e2eec28967f844c74c6e0f8442
杉浦智大 <sugiuratomohiro@sugiurasatoshidainoMacBook-Pro.local>
2019-06-10 17:43:05 +0900

    groups action

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,4 +1,5 @@
 class GroupsController < ApplicationController
+  before_action :set_group,only: [:edit,:update] 
 def new
   @group = Group.new
   @group.users << current_user
@@ -14,14 +15,23 @@ def create
 end
 
 def edit
+  
 end
 
 def update
+  if @group.update(group_params)
+    redirect_to group_messages_path(@group),notice: "グループを編集しました"
+  else
+    render :edit
+  end
 end
 
 private
 def group_params
   params.require(:group).permit(:name, { :user_ids => [] })
 end
+
+def set_group
+  @group = Group.find(params[:id])
 
 end
