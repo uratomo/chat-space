26e515a2643d8c4bedcf1b44a2aafcf77ef919d3
杉浦智大 <sugiuratomohiro@sugiurasatoshidainoMacBook-Pro.local>
2019-06-10 15:59:57 +0900

    model fixed

--- db/migrate/20190610052539_create_group_users.rb
+++ db/migrate/20190610052539_create_group_users.rb
@@ -1,8 +1,8 @@
 class CreateGroupUsers < ActiveRecord::Migration[5.0]
   def change
     create_table :group_users do |t|
-      t.reference :group , foreign_key: true
-      t.reference :user,foreign_key: true
+      t.references :group , foreign_key: true
+      t.references :user,foreign_key: true
       t.timestamps
     end
   end
